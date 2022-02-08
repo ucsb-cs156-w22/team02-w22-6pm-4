a3d9175eb300b93844bf43596774123b5f658ebd
rsdharmaji <59861195+rsdharmaji@users.noreply.github.com>
2022-02-07 18:31:13 -0800

    rd - add CSRF mapping (from prof. conrad)

--- src/main/java/edu/ucsb/cs156/team02/controllers/FrontendController.java
+++ src/main/java/edu/ucsb/cs156/team02/controllers/FrontendController.java
@@ -1,6 +1,7 @@
-package edu.ucsb.cs156.team02.controllers;
+package edu.ucsb.cs156.example.controllers;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,4 +12,35 @@ public class FrontendController {
   public String index() {
     return "forward:/index.html";
   }
+
+  @GetMapping("/csrf")
+  public ResponseEntity<String> csrf() {
+    return ResponseEntity.notFound().build();
+  }
+
 }
+
+
+
+
+//package edu.ucsb.cs156.team02.controllers;
+
+
+
+
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.http.ResponseEntity;
+//@Profile("!development")
+//@Controller
+//public class FrontendController {
+  //@GetMapping("/**/{path:[^\\.]*}")
+  //public String index() {
+    //return "forward:/index.html";
+  //}
+  //@GetMapping("/csrf")
+  //public ResponseEntity<String> csrf() {
+    //return ResponseEntity.notFound().body("/csrf not implemented");
+  //}
+//}
